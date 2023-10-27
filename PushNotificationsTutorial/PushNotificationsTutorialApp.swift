9d2ffaad2c28a6bf3ee4f2f2bf70b56f7d629e42
Julian Worden <93101937+julianworden@users.noreply.github.com>
2023-10-27 11:43:37 -0700

    Set Up Firebase

--- PushNotificationsTutorial/PushNotificationsTutorialApp.swift
+++ PushNotificationsTutorial/PushNotificationsTutorialApp.swift
@@ -5,13 +5,25 @@
 //  Created by Julian Worden on 10/27/23.
 //
 
+import FirebaseCore
 import SwiftUI
 
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        FirebaseApp.configure()
+        return true
+    }
+}
+
 @main
-struct PushNotificationsTutorialApp: App {
+struct YourApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            NavigationView {
+                ContentView()
+            }
         }
     }
 }
