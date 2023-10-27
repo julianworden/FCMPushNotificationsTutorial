401282c26289ad4c14c1a6325bed1159c6837e55
Julian Worden <93101937+julianworden@users.noreply.github.com>
2023-10-27 11:24:04 -0700

    Initial Commit

---
+++ PushNotificationsTutorial/PushNotificationsTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  PushNotificationsTutorialApp.swift
+//  PushNotificationsTutorial
+//
+//  Created by Julian Worden on 10/27/23.
+//
+
+import SwiftUI
+
+@main
+struct PushNotificationsTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
